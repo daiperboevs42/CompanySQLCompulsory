e8fbc2bdad5bd117e64d29379da3332cdbce5003
Martin Emil Wøbbe <54886017+daiperboevs42@users.noreply.github.com>
2022-03-08 22:37:16 +0100

    initial commit

---
+++ CompanySQLCompulsory/CompanySQLCompulsory.Core.Entity/DEPARTMENT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanySQLCompulsory.CompanySQLCompulsory.Core.Entity
+{
+    public class DEPARTMENT
+    {
+        public int Dnumber { get; set; }
+        public string Dname { get; set; }
+        public int Mgr_ssn { get; set; }
+        public DateTime Mgr_start_date { get; set; }
+        public string Dlocation { get; set; }
+    }
+}
---
+++ CompanySQLCompulsory/CompanySQLCompulsory.GUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompanySQLCompulsory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
